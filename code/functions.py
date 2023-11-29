0767261e1ee02928a6519fdeef6094dac10d0e53
Ali <mohammadalimir110@gmail.com>
2023-11-29 11:52:49 -0800

    Update tests for functions.py

--- code/functions.py
+++ code/functions.py
@@ -1,12 +1,45 @@
 import numpy as np
+import pandas as pd
 
 
-def iqr(list=[], scale=1.5):
+def iqr_limits(data=[], scale=1.5):
+    if isinstance(data, pd.Series):
+        data = data.tolist()  # Convert Series to list
+    if len(data) == 0:
+        return None
+    if not isinstance(scale, (float, int)) or scale < 0:
+        scale = 1.5
 
-    q1 = np.quantile(list, 0.25)
-    q3 = np.quantile(list, 0.75)
-    iqr = q3-q1
-    low_lim = q1 - scale*iqr
-    high_lim = q3 + scale*iqr
+    try:
+        q1 = np.quantile(data, 0.25)
+        q3 = np.quantile(data, 0.75)
+        iqr = q3 - q1
+        low_lim = q1 - scale * iqr
+        high_lim = q3 + scale * iqr
+        limits = [low_lim, high_lim]
+        return limits
+    except Exception:
+        return None
 
-    return low_lim, high_lim
+
+def historical_extremes(data, history_data, sensitivity=10):
+    if len(data) == 0 or len(history_data) == 0:
+        return pd.Series(None, None, dtype='float64')
+    if not isinstance(sensitivity, (float, int)) or sensitivity < 0:
+        sensitivity = 10
+    if sensitivity < 0:
+        sensitivity = -sensitivity
+    if isinstance(data, list):
+        data = pd.Series(data)
+    if isinstance(history_data, list):
+        history_data = pd.Series(history_data)
+
+    flags = np.where((data < history_data - sensitivity) |
+                     (data > history_data + sensitivity),
+                     'Extreme', 'Normal')
+
+    extreme_indices = np.where(flags == 'Extreme')[0]
+    extreme_data = data[extreme_indices]
+
+    return extreme_data.astype('float')
+################################################
--- code/test_functions.py
+++ code/test_functions.py
@@ -1,10 +1,42 @@
 import pytest
 import functions
+# from functions import *
 
 
-@pytest.mark.parametrize("one,two",
-                         [("R2D2", False),
-                          # R2D2 is too short
-                          ])
-def test_functions(data, expected):
-    assert functions.iqr_limits(data) == expected
+@pytest.mark.parametrize("data, scale, limits", [
+    ([-20, -2, 0, 1, 2, 20], [], [-6.375, 6.625]),  # scale=1.5
+    ([-20, -2, 0, 1, 2, 20], 1.5, [-6.375, 6.625]),
+    ([-20, -2, 0, 1, 2, 20], 2, [-8, 8.25]),  # scale=2
+    ([-20, -2, 0, 1, 2, 20], -1, [-6.375, 6.625]),
+    ([0], [], [0, 0]),
+    ([], [], None),
+    (['a', 'b', 'c'], [], None),
+    (functions.pd.Series([1, 2, 3, 4, 5]), [], [-1, 7])  # Series
+])
+def test_iqr_limits(data, scale, limits):
+    result = functions.iqr_limits(data, scale)
+    assert result == limits
+
+
+@pytest.mark.parametrize("data, history_data, sensitivity,\
+                         extremes_values, extremes_indexes", [
+    ([5, 10, 3, 0, -10, 20], [6, 9, 1, 2, 5.5, 4], 10, [-10, 20], [4, 5]),
+    ([5, 10, 3, 0, -10, 20], [6, 9, 1, 2, 5.5, 4], None, [-10, 20], [4, 5]),
+    # sensitivity: default
+    ([5, 10, 3, 0, -10, 20], [6, 9, 1, 2, 5.5, 4], -10, [-10, 20], [4, 5]),
+    # sensitivity: negative
+    ([5, 10, 3, 0, -10, 20], [], None, None, None),
+    ([], [6, 9, 1, 2, 5.5, 4], None, None, None),
+    ([5, 10, 3, 0, 0, 9.5, 10], [6, 9, 1, 2, 5.5, 4, 10], 5, [0, 9.5], [4, 5]),
+    # sensitivity: five
+    ([0, 10, 15, 20, 5], [6, 12, 10, 14, 9], 5, [0, 20], [0, 3]),
+    ([0], [6], 5, [0], [0])
+])
+def test_historical_extremes(data, history_data, sensitivity,
+                             extremes_values, extremes_indexes):
+    result = functions.historical_extremes(data, history_data, sensitivity)
+    expected = functions.pd.Series(extremes_values, extremes_indexes,
+                                   dtype='float64')
+    assert result.equals(expected)
+###################################################################
+# python -m pytest -v test_functions.py::test_historical_extremes
